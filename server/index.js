2328584a60683e00268257239a93f4323a1bb983
Namisami <79804458+Namisami@users.noreply.github.com>
2024-09-01 15:29:37 +0300

    fix cors error

--- server/index.js
+++ server/index.js
@@ -9,6 +9,8 @@ router.add('/albums', albumRouter);
 router.add('/artists', artistRouter);
 
 const app = http.createServer((req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader("Content-Type", "application/json; charset=utf-8");
   router.execute(req, res);
 });
